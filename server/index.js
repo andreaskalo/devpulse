0bd09ad74edf6a3a13854acca1cee9dc6ccef328
Andreas Kalo <andreaskalo001@gmail.com>
2026-09-09 15:19:34 +0300

    create register route, add bcrypt for password hash

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import pool from "./db/index.js";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json()); // used in order to read/parse json data from react front-end
+
+app.use("/api/auth", authRouter);
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ status: "ok" });
